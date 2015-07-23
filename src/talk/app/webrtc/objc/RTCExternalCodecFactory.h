e0151f530b729c7af9105ebd98f0bd62e5cb55ec
Heberth Braga <heberthbraga@Heberths-MBP.home>
2015-07-23 12:07:23 -0300

    Revert "Changed header file for RTCExternalCodecFactory"
    This reverts commit dcd949a33a749ff9b1e1c94f660f2be72404beb6.

--- src/talk/app/webrtc/objc/RTCExternalCodecFactory.h
+++ src/talk/app/webrtc/objc/RTCExternalCodecFactory.h
@@ -1,6 +1,3 @@
-#ifndef TALK_APP_WEBRTC_OBJC_RTC_EXTERNAL_CODEC_FACTORY_H_
-#define TALK_APP_WEBRTC_OBJC_RTC_EXTERNAL_CODEC_FACTORY_H_
-
 #import <Foundation/Foundation.h>
 
 #include "webrtc/base/scoped_ptr.h"
@@ -12,5 +9,3 @@
 	- (rtc::scoped_ptr<cricket::WebRtcVideoEncoderFactory>) getEncoderFactory;
 	- (rtc::scoped_ptr<cricket::WebRtcVideoDecoderFactory>) getDecoderFactory;
 @end
-
-#endif
